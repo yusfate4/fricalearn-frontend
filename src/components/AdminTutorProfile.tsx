5b2389b25f499487d763682168a3027637f256bd
yusfate4 <abiodunyusuf4@gmail.com>
2026-04-11 22:28:37 +0100

    New login for super admin account

--- src/components/AdminTutorProfile.tsx
+++ src/components/AdminTutorProfile.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Layout from "../../components/Layout";
-import api from "../../api/axios";
+// import Layout from "../../components/Layout";
+import Layout from "./Layout";
+import api from "../api/axios";
 import { 
   UserCircle, 
   BookOpen, 
