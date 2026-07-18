a9c8fa4da8e6c511ba9b35a506f6c6091688aa64
yusfate4 <abiodunyusuf4@gmail.com>
2026-07-18 15:04:56 +0100

    logo modified

--- src/pages/LandingPage/Header.tsx
+++ src/pages/LandingPage/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC = () => {
 
         {/* Logo */}
         <Link to="/" className="flex items-center gap-2 group">
-          <img src="src/assets/logo.png" alt="FricaLearn"
+          <img src="logo.png" alt="FricaLearn"
             className="h-10 md:h-12 w-auto object-contain transition-transform group-hover:scale-105" />
         </Link>
 
